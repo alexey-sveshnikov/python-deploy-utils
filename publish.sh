e2064727b2faabcec57fd2e33937dbf6ed39aee8
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2014-07-29 12:55:27 +0400

    fixed typo in pip install devpi command. Date-based version.

--- publish.sh
+++ publish.sh
@@ -6,6 +6,14 @@ REPOSITORY_HOST='repo0'
 #version=`head -n1 debian/changelog | cut -f2 -d\( | cut -f1 -d\)`
 version=`date +"%Y-%m-%d--%H-%M" | tr -d "\n"`
 
+cat > debian/changelog <<END
+$PACKAGE ($version) unstable; urgency=low
+
+  * Nothing here
+
+ -- Alexey <alexey@futubank.com>  Fri, 04 Apr 2013 11:52:18 +0400
+END
+
 pdebuild --buildresult .. --debbuildopts '-us -uc'
 
 deb_file=${PACKAGE}_${version}_amd64.deb
